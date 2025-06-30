3b2c648fc38c350c8c1e85d4abd1141156b283ca
renxinxing123 <renxinxing1@gmail.com>
2025-06-30 16:48:05 +0100

    update .gitignore

--- env.fish
+++
@@ -1,4 +0,0 @@
-if not contains "$HOME/coral/coral-orchestrator/Coral-Monzo-Agent" $PATH
-    # Prepending path in case a system-installed binary needs to be overridden
-    set -x PATH "$HOME/coral/coral-orchestrator/Coral-Monzo-Agent" $PATH
-end
